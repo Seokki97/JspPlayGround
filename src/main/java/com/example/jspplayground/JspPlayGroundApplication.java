932b5054171440c6654413989be4f04766cc5f55
Seokki97 <skaksdl1238@gmail.com>
2024-03-20 13:06:04 +0900

    docs - JSP PLAYGROUND

---
+++ src/main/java/com/example/jspplayground/JspPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.jspplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspPlayGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspPlayGroundApplication.class, args);
+	}
+
+}
